c18ef5610c839c0ef9edfd67053ee4056690ba4c
slux <slux83@gmail.com>
2018-08-21 21:44:52 +0200

    added real certificate

--- src/main/java/de/slux/line/jarvis/JarvisBotApplication.java
+++ src/main/java/de/slux/line/jarvis/JarvisBotApplication.java
@@ -133,32 +133,6 @@ public class JarvisBotApplication {
 		return this.lineMessagingClient;
 	}
 
-	@Bean
-	public EmbeddedServletContainerFactory servletContainer() {
-		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
-			@Override
-			protected void postProcessContext(Context context) {
-				SecurityConstraint securityConstraint = new SecurityConstraint();
-				securityConstraint.setUserConstraint("CONFIDENTIAL");
-				SecurityCollection collection = new SecurityCollection();
-				collection.addPattern("/*");
-				securityConstraint.addCollection(collection);
-				context.addConstraint(securityConstraint);
-			}
-		};
-
-		tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
-		return tomcat;
-	}
-
-	private Connector initiateHttpConnector() {
-		Connector connector = new Connector(
-				"org.apache.coyote.http11.Http11NioProtocol");
-		connector.setScheme("http");
-		connector.setPort(8080);
-		connector.setSecure(false);
-		connector.setRedirectPort(8443);
-
-		return connector;
-	}
 }
+
+
--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-PORT=50331
 MAIN="de.slux.line.jarvis.JarvisBotApplication"
 JAVA_OPTS=""
 LINE_CHANNEL_TOKEN="ujk+j34cFZyA9qW0DYhKJ8P7+6FkmnEyWbcu7xhUmwhwVanjpjalsUfVgtHcrnXfhiAV2VIRIC0wAyYF7BBFvoKaenklkRi38/F+34qcyBXQVFMgTyrlmp8ILBLVgduDHvTehU9yf1RwOQ71cSBXhAdB04t89/1O/w1cDnyilFU="
@@ -10,6 +9,5 @@ java -Dline.bot.channelSecret="$LINE_CHANNEL_SECRET" \
 	-Dline.bot.channelToken="$LINE_CHANNEL_TOKEN" $JAVA_OPTS \
 	-cp "target/jarvis-bot-0.0.1-SNAPSHOT.jar:target/libs/*" ${MAIN} 
 
-#--server.port=$PORT
 
 
