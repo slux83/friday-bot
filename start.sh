643eaa87f604bc0c1e06559c959927f46756c289
Alessio Di Fazio <Test-Val.service.ADiFazio@esa.int>
2018-08-21 10:15:52 +0000

    added channel token/secret in startup script
    Signed-off-by: Alessio Di Fazio <Test-Val.service.ADiFazio@esa.int>

--- start.sh
+++ start.sh
@@ -3,8 +3,11 @@
 PORT=50331
 MAIN="de.slux.line.jarvis.JarvisBotApplication"
 JAVA_OPTS=""
+LINE_CHANNEL_TOKEN="ujk+j34cFZyA9qW0DYhKJ8P7+6FkmnEyWbcu7xhUmwhwVanjpjalsUfVgtHcrnXfhiAV2VIRIC0wAyYF7BBFvoKaenklkRi38/F+34qcyBXQVFMgTyrlmp8ILBLVgduDHvTehU9yf1RwOQ71cSBXhAdB04t89/1O/w1cDnyilFU="
+LINE_CHANNEL_SECRET="a3cabd40aa88ddcfdeaecfabcd43dd9a"
 
-java $JAVA_OPTS -cp "target/jarvis-bot-0.0.1-SNAPSHOT.jar:target/libs/*" ${MAIN} --server.port=$PORT
+java -Dline.bot.channelSecret="$LINE_CHANNEL_SECRET" \
+	-Dline.bot.channelToken="$LINE_CHANNEL_TOKEN" $JAVA_OPTS \
+	-cp "target/jarvis-bot-0.0.1-SNAPSHOT.jar:target/libs/*" ${MAIN} --server.port=$PORT
 
-#jarvis-bot-0.0.1-SNAPSHOT.jar
-#src/main/java/de/slux/line/jarvis/JarvisBotApplication.java
+
