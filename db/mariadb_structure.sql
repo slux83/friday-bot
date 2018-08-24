514100341645294e1d802edffec31f4888509b2d
slux <slux83@gmail.com>
2018-08-24 16:22:40 +0200

    fixed small issues with DB types
    Signed-off-by: slux <slux83@gmail.com>

--- db/mariadb_structure.sql
+++ db/mariadb_structure.sql
@@ -20,10 +20,10 @@ USE `jarvis`;
 CREATE TABLE IF NOT EXISTS `war_death` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `group_id` int(11) NOT NULL DEFAULT 0,
-  `node` int(11) NOT NULL DEFAULT 0,
-  `num_deaths` int(11) NOT NULL DEFAULT 0,
-  `champion` varchar(128) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
-  `player` varchar(128) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  `node` tinyint(4) NOT NULL DEFAULT 0,
+  `num_deaths` tinyint(4) NOT NULL DEFAULT 0,
+  `champion` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `player` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_war_death_war_group` (`group_id`),
   CONSTRAINT `FK_war_death_war_group` FOREIGN KEY (`group_id`) REFERENCES `war_group` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
@@ -34,10 +34,10 @@ CREATE TABLE IF NOT EXISTS `war_death` (
 CREATE TABLE IF NOT EXISTS `war_group` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `group_id` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
-  `group_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  `group_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `group_id` (`group_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- Data exporting was unselected.
 -- Dumping structure for table jarvis.war_history
@@ -46,10 +46,10 @@ CREATE TABLE IF NOT EXISTS `war_history` (
   `war_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `opponent_tag` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `group_id` int(11) NOT NULL DEFAULT 0,
-  `node` int(11) NOT NULL DEFAULT 0,
-  `num_deaths` int(11) NOT NULL DEFAULT 0,
-  `champion` varchar(128) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
-  `player` varchar(128) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  `node` tinyint(4) NOT NULL DEFAULT 0,
+  `num_deaths` tinyint(4) NOT NULL DEFAULT 0,
+  `champion` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `player` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_war_history_war_group` (`group_id`),
   CONSTRAINT `FK_war_history_war_group` FOREIGN KEY (`group_id`) REFERENCES `war_group` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
@@ -59,24 +59,24 @@ CREATE TABLE IF NOT EXISTS `war_history` (
 -- Dumping structure for table jarvis.war_placement
 CREATE TABLE IF NOT EXISTS `war_placement` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `summoner_id` int(11) NOT NULL DEFAULT 0,
-  `node` int(11) NOT NULL DEFAULT 0,
-  `champ` varchar(128) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  `summoner_id` int(11) NOT NULL,
+  `node` tinyint(4) NOT NULL DEFAULT 0,
+  `champ` varchar(128) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_war_placement_war_summoner` (`summoner_id`),
   CONSTRAINT `FK_war_placement_war_summoner` FOREIGN KEY (`summoner_id`) REFERENCES `war_summoner` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
 -- Data exporting was unselected.
 -- Dumping structure for table jarvis.war_summoner
 CREATE TABLE IF NOT EXISTS `war_summoner` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `group_id` int(11) NOT NULL DEFAULT 0,
-  `name` varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  `group_id` int(11) NOT NULL,
+  `name` varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_war_summoner_war_group` (`group_id`),
   CONSTRAINT `FK_war_summoner_war_group` FOREIGN KEY (`group_id`) REFERENCES `war_group` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 -- Data exporting was unselected.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
