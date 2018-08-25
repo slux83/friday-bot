1563103b6e1fbfe0b7b49b24804e55632bc31190
slux <slux83@gmail.com>
2018-08-25 20:06:00 +0200

    edited start.sh

--- start.sh
+++ start.sh
@@ -4,12 +4,24 @@ MAIN="de.slux.line.jarvis.JarvisBotApplication"
 JAVA_OPTS=""
 LINE_CHANNEL_TOKEN="ujk+j34cFZyA9qW0DYhKJ8P7+6FkmnEyWbcu7xhUmwhwVanjpjalsUfVgtHcrnXfhiAV2VIRIC0wAyYF7BBFvoKaenklkRi38/F+34qcyBXQVFMgTyrlmp8ILBLVgduDHvTehU9yf1RwOQ71cSBXhAdB04t89/1O/w1cDnyilFU="
 LINE_CHANNEL_SECRET="a3cabd40aa88ddcfdeaecfabcd43dd9a"
-JARVIS_VERSION="0.0.1-beta1"
+JARVIS_VERSION="0.0.1-beta2"
+DB_USER="jarvis"
+DB_PASSWORD="jarvis"
+DB_HOST="localhost"
+DB_PORT="3306"
+DB_NAME="jarvis"
+
 
 echo "*** Starting Jarvis BOT ${JARVIS_VERSION} ***"
 
-java -Dline.bot.channelSecret="$LINE_CHANNEL_SECRET" \
-	-Dline.bot.channelToken="$LINE_CHANNEL_TOKEN" $JAVA_OPTS \
+java    -Dline.bot.channelSecret="$LINE_CHANNEL_SECRET" \
+	-Dline.bot.channelToken="$LINE_CHANNEL_TOKEN" \
+	-Djarvis.db.user="$DB_USER" \
+	-Djarvis.db.password="$DB_PASSWORD" \
+	-Djarvis.db.host="$DB_HOST" \
+	-Djarvis.db.port="$DB_PORT" \
+	-Djarvis.db.name="$DB_NAME" \
+	$JAVA_OPTS \
 	-cp "target/jarvis-bot-${JARVIS_VERSION}.jar:target/libs/*" ${MAIN} 
 
 
