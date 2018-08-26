ae80d94a2c540de198c35a97037646315ef47af8
slux <slux83@gmail.com>
2018-08-26 21:11:01 +0200

    small refactoring on tests
    Signed-off-by: slux <slux83@gmail.com>

---
+++ src/test/java/de/slux/line/jarvis/test/command/TestUtilityCommand.java
@@ -0,0 +1,97 @@
+/**
+ * 
+ */
+package de.slux.line.jarvis.test.command;
+
+import static org.junit.Assert.assertTrue;
+
+import java.time.Instant;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.event.source.GroupSource;
+import com.linecorp.bot.model.event.source.Source;
+import com.linecorp.bot.model.event.source.UserSource;
+import com.linecorp.bot.model.message.TextMessage;
+
+import de.slux.line.jarvis.JarvisBotApplication;
+import de.slux.line.jarvis.command.InfoCommand;
+import de.slux.line.jarvis.command.admin.AdminStatusCommand;
+import de.slux.line.jarvis.test.util.LineMessagingClientMock;
+import de.slux.line.jarvis.test.util.MessagingClientCallbackImpl;
+
+/**
+ * @author slux
+ */
+public class TestUtilityCommand {
+
+	@Test
+	public void testAdminStatusCommand() throws Exception {
+
+		MessagingClientCallbackImpl callback = new MessagingClientCallbackImpl();
+		JarvisBotApplication jarvis = new JarvisBotApplication(null);
+		jarvis.setLineMessagingClient(new LineMessagingClientMock(callback));
+		jarvis.postConstruct();
+
+		Source source = new UserSource(JarvisBotApplication.SLUX_ID);
+		Instant timestamp = Instant.now();
+		TextMessageContent message = new TextMessageContent("001", AdminStatusCommand.CMD_PREFIX);
+		MessageEvent<TextMessageContent> event = new MessageEvent<TextMessageContent>("reply-token", source, message,
+		        timestamp);
+
+		jarvis.getIncomingMsgCounter().set(10);
+		Thread.sleep(1000);
+		TextMessage response = jarvis.handleTextMessageEvent(event);
+
+		System.out.println(response);
+
+		message = new TextMessageContent("001", AdminStatusCommand.CMD_PREFIX + " fake foo bar");
+		event = new MessageEvent<TextMessageContent>("reply-token", source, message, timestamp);
+		response = jarvis.handleTextMessageEvent(event);
+
+		Assert.assertTrue(response.getText().contains("status fake"));
+
+		// 16 secs day
+		System.out.println(AdminStatusCommand.calculateUptime(1000 * 16));
+
+		// 1 day
+		System.out.println(AdminStatusCommand.calculateUptime(1000 * 60 * 60 * 24));
+
+		// 10 day
+		System.out.println(AdminStatusCommand.calculateUptime(1000 * 60 * 60 * 24 * 10));
+
+		// 10 day and 1h
+		System.out.println(AdminStatusCommand.calculateUptime((1000 * 60 * 60 * 24 * 10) + 1000 * 60 * 60));
+
+		// 10 day and 1h and 12 min
+		System.out.println(
+		        AdminStatusCommand.calculateUptime(((1000 * 60 * 60 * 24 * 10) + 1000 * 60 * 60) + 1000 * 60 * 12));
+
+		// 10 day and 1h and 12 min and 40 secs
+		System.out.println(AdminStatusCommand
+		        .calculateUptime((((1000 * 60 * 60 * 24 * 10) + 1000 * 60 * 60) + 1000 * 60 * 12) + 1000 * 40));
+
+	}
+
+	@Test
+	public void testInfoCommand() throws Exception {
+		MessagingClientCallbackImpl callback = new MessagingClientCallbackImpl();
+		JarvisBotApplication jarvis = new JarvisBotApplication(null);
+		jarvis.setLineMessagingClient(new LineMessagingClientMock(callback));
+		jarvis.postConstruct();
+
+		Source source = new GroupSource("group-id", "user-id");
+		Instant timestamp = Instant.now();
+		TextMessageContent message = new TextMessageContent("001", InfoCommand.CMD_PREFIX);
+		MessageEvent<TextMessageContent> event = new MessageEvent<TextMessageContent>("reply-token", source, message,
+		        timestamp);
+
+		TextMessage response = jarvis.handleTextMessageEvent(event);
+
+		assertTrue(response.getText().contains("J.A.R.V.I.S. MCOC Line Bot"));
+	}
+
+}
