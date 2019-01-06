4d2de15ea3ce9f712162259f677e3cfb80a2d023
slux <slux83@gmail.com>
2019-01-06 18:15:45 +0100

    working on #28
    Signed-off-by: slux <slux83@gmail.com>

--- src/main/java/de/slux/line/friday/command/war/WarDiversityCommand.java
+++ src/main/java/de/slux/line/friday/command/war/WarDiversityCommand.java
@@ -4,9 +4,11 @@
 package de.slux.line.friday.command.war;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,8 +19,10 @@ import com.linecorp.bot.model.message.TextMessage;
 import de.slux.line.friday.command.AbstractCommand;
 import de.slux.line.friday.command.HelpCommand;
 import de.slux.line.friday.dao.exception.WarDaoUnregisteredException;
+import de.slux.line.friday.data.war.WarDeath;
 import de.slux.line.friday.data.war.WarGroup;
 import de.slux.line.friday.data.war.WarSummoner;
+import de.slux.line.friday.data.war.WarSummonerPlacement;
 import de.slux.line.friday.logic.StatsLogic;
 import de.slux.line.friday.logic.war.WarDeathLogic;
 import de.slux.line.friday.logic.war.WarPlacementLogic;
@@ -70,14 +74,30 @@ public class WarDiversityCommand extends AbstractCommand {
 			Map<Integer, WarSummoner> placementTable = warPlacementModel.getSummoners(senderId);
 			WarGroup deathTable = warModel.getSummaryModel(senderId);
 
-			// TODO: get the map of champ_inserted -> node
+
+			// build the map of node -> champs
+			Map<Integer, List<String>> allReports = new HashMap<>();
+			for (Entry<Integer, WarSummoner> entry : placementTable.entrySet()) {
+				for (Entry<Character, WarSummonerPlacement> placement : entry.getValue().getPlacements().entrySet()) {
+					if (!allReports.containsKey(placement.getValue().getNode())) {
+						allReports.put(placement.getValue().getNode(), new ArrayList<>());
+					}
+					allReports.get(placement.getValue().getNode()).add(placement.getValue().getChampion());
+				}
+			}
+
+			for (WarDeath deathReport : deathTable.getDeathReports()) {
+				if (!allReports.containsKey(deathReport.getNodeNumber())) {
+					allReports.put(deathReport.getNodeNumber(), new ArrayList<>());
+				}
+				allReports.get(deathReport.getNodeNumber()).add(deathReport.getChampName());
+			}
+
+			// XXX 
 			// TODO: scan the map and guess the champ
 			// TODO: build the diversity analysis and log mismatches between
 			// deaths and summoner table if any (same node, different champ
 			// guessed)
-
-			// Champ, nodes
-			Map<String, List<Integer>> allReports = new HashMap<>();
 
 		} catch (WarDaoUnregisteredException e) {
 			return new TextMessage("This group is unregistered! Please use '" + AbstractCommand.ALL_CMD_PREFIX + " "
