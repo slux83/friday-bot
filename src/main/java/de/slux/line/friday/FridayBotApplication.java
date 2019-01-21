42f1d0e0423906376d3067305639196febe307de
slux <slux83@gmail.com>
2019-01-21 13:50:41 +0100

    Moved to version 0.1.1
    Signed-off-by: slux <slux83@gmail.com>

--- src/main/java/de/slux/line/friday/FridayBotApplication.java
+++ src/main/java/de/slux/line/friday/FridayBotApplication.java
@@ -91,7 +91,7 @@ import de.slux.line.friday.scheduler.McocSchedulerImporter;
 @SpringBootApplication
 @LineMessageHandler
 public class FridayBotApplication {
-	public static String FRIDAY_VERSION = "0.1.0";
+	public static String FRIDAY_VERSION = "0.1.1";
 	public static final int MAX_LINE_MESSAGE_SIZE = 1500;
 
 	private static Logger LOG = LoggerFactory.getLogger(FridayBotApplication.class);
