532b63e2ab38d3fb64e05293b61f481b891fcf9f
Alessio di Fazio <slux83@gmail.com>
2020-05-13 16:10:21 +0200

    updated line chat link
    Signed-off-by: Alessio di Fazio <slux83@gmail.com>

--- src/main/java/de/slux/line/friday/command/InfoCommand.java
+++ src/main/java/de/slux/line/friday/command/InfoCommand.java
@@ -17,7 +17,7 @@ public class InfoCommand extends AbstractCommand {
 
     // Replace $GUID with the groupID
     private static final String PAYPAL_LINK = "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=slux83@gmail.com&lc=US&item_name=friday%3A$GUID&no_note=0&cn=&currency_code=USD&bn=PP-DonationsBF:btn_donateCC_LG.gif:NonHosted";
-    private static final String FRIDAY_USERS_LINE_GROUP_LINK = "https://line.me/R/ti/g/oNF5Riui79";
+    private static final String FRIDAY_USERS_LINE_GROUP_LINK = "https://line.me/R/ti/g/l2pd-jBqcD";
 
     /**
      * Ctor
