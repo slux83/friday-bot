2c42f83ceb10de546ecd39b27951d2a9b97212ce
Alessio di Fazio <slux83@gmail.com>
2020-05-13 15:29:13 +0200

    new version 0.2.3
    Signed-off-by: Alessio di Fazio <slux83@gmail.com>

--- src/main/java/de/slux/line/friday/FridayBotApplication.java
+++ src/main/java/de/slux/line/friday/FridayBotApplication.java
@@ -65,7 +65,7 @@ import java.util.stream.Collectors;
 @SpringBootApplication
 @LineMessageHandler
 public class FridayBotApplication {
-    public static String FRIDAY_VERSION = "0.2.2";
+    public static String FRIDAY_VERSION = "0.2.3";
     public static final int MAX_LINE_MESSAGE_SIZE = 1500;
 
     private static Logger LOG = LoggerFactory.getLogger(FridayBotApplication.class);
